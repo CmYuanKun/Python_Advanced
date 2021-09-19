b00fda64d5b3dea16b9c6a746751ab01cfa865ab
CmYuanKun <474893246@qq.com>
2021-09-19 15:34:27 +0800

    2021/9/19提交

--- 01_hello.py/Linked_list.py
+++ 01_hello.py/Linked_list.py
@@ -1,8 +1,20 @@
+#coding:utf-8
 '''
 Author: your name
 Date: 2021-09-17 23:37:29
-LastEditTime: 2021-09-19 00:48:46
+LastEditTime: 2021-09-19 11:03:12
 LastEditors: Please set LastEditors
 Description: In User Settings Edit
 FilePath: \Git_Practice_Code\hello_world.py
 '''
+class Node(object):
+    """节点"""
+    def __init__(self, elem):
+        self.elem = elem
+        self.next = None
+
+class singleLinkList(object):
+    """单链表"""
+    def __init__(self) :
+        self.Node = Node()
+         
